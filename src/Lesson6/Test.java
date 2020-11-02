0eb246af9fa95161a62ccf56ee77001fda65e0f5
geekbrains <niki-babkin@mail.ru>
2020-11-02 18:54:51 +0300

    ДЗ за шестой урок

---
+++ src/Lesson6/Test.java
@@ -0,0 +1,11 @@
+package Lesson6;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+        Dog dog1 = new Dog(500, 0.5, 10);
+        Cat cat1 = new Cat(200, 2, 0);
+
+    }
+}
